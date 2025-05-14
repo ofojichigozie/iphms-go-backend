a69dff6a1626e713c43a910e5c01a8d91e8c0725
Chigozie Ofoji <ofojichigozie@gmail.com>
2025-05-14 14:40:06 +0100

    Updated environment loading to support production deployment

--- initializers/load_env_variables.go
+++ initializers/load_env_variables.go
@@ -2,13 +2,16 @@ package initializers
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func LoadEnvVariables() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+	if os.Getenv("APP_ENV") != "production" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
 	}
 }
